fe450e6801c57fa948eff75bc1a744db3797b7d0
MrCraft18 <mariocaden@gmail.com>
2024-03-02 09:41:40 -0600

    Finished getGroupPosts function but need to add scraping for post date

---
+++ fb-lib.js
@@ -0,0 +1,196 @@
+const puppeteer = require('puppeteer')
+
+const fb = {
+    login: (user, pass) => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                if (!user) return reject('Missing Email/Phone Number Field')
+                if (!pass) return reject('Missing Password Field')
+
+                browser = await puppeteer.launch({
+                    headless: 'new',
+                    userDataDir: './browser'
+                })
+
+                const page = await browser.newPage()
+                await page.setViewport({ width: 1920, height: 1080 })
+
+                await Promise.all([
+                    page.goto('https://www.facebook.com/'),
+                    page.waitForNavigation()
+                ])
+
+                const loginButton = await page.$('button')
+
+                if (loginButton) {
+                    const emailInput = await page.$('#email')
+                    await emailInput.type(user)
+
+                    const passInput = await page.$('#pass')
+                    await passInput.type(pass)
+
+                    await Promise.all([
+                        loginButton.click(),
+                        page.waitForNavigation()
+                    ])
+
+                    resolve()
+
+                    page.close()
+                } else {
+                    resolve()
+
+                    page.close()
+                }
+            } catch (error) {
+                reject(error)
+            }
+        })
+    },
+    getJoinedGroups: () => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const page = await browser.newPage()
+                await page.setViewport({ width: 1920, height: 1080 })
+
+                await Promise.all([
+                    page.goto('https://www.facebook.com/groups/joins/?nav_source=tab'),
+                    page.waitForNavigation()
+                ])
+
+                const groups = await page.evaluate(() => {
+                    const span = Array.from(document.querySelectorAll('span'))
+                    .find(element => element.innerText.includes("All groups you've joined"))
+
+                    const bigParent = span.closest('.x9f619.x1gryazu.xkrivgy.x1ikqzku.x1h0ha7o.xg83lxy.xh8yej3')
+
+                    const listParent = Array.from(bigParent.querySelectorAll('*'))
+                    .find(element => element.matches('.x8gbvx8.x78zum5.x1q0g3np.x1a02dak.x1nhvcw1.x1rdy4ex.xcud41i.x4vbgl9.x139jcc6'))
+                    
+                    const anchorElements = Array.from(listParent.querySelectorAll('a.x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1heor9g.xt0b8zv.x1s688f'))
+
+                    groupObjects = anchorElements.map(element => ({
+                        name: element.innerText,
+                        id: element.href.split('/').at(-2),
+                        link: element.href
+                    }))
+
+                    return groupObjects
+                })
+
+                resolve(groups)
+
+                page.close()
+            } catch (error) {
+                reject(error)
+            }
+        })
+    },
+    getGroupPosts: (id, limit) => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const page = await browser.newPage()
+                await page.setViewport({ width: 1920, height: 1080 })
+
+                await Promise.all([
+                    page.goto(`https://www.facebook.com/groups/${id}?sorting_setting=CHRONOLOGICAL`),
+                    page.waitForNavigation()
+                ])
+
+                const posts = []
+
+                for (let i = 0; i < limit; i++) {
+                    await page.waitForFunction(index => {
+                        const elements = Array.from(document.querySelectorAll(`[role="feed"] > .x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z`))
+                        return elements[index]
+                    }, { polling: 'mutation' }, i)
+
+                    const postElements = await page.$$(`[role="feed"] > .x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z`)
+                    let postElement = postElements[i]
+
+                    await postElement.hover()
+
+                    //Wait for Author Name Element
+                    await postElement.waitForSelector('a[aria-label]')
+
+                    // page.on('console', msg => {
+                    //     console.log('PAGE LOG:', msg.text());
+                    // })
+
+                    const post = await postElement.evaluate(element => {
+                        return new Promise(async (resolve) => {
+                            const seeMoreButton = Array.from(element.querySelectorAll('div')).find(element => element.innerText === 'See more' && !element.closest('.xzueoph'))
+                            if (seeMoreButton) {
+                                seeMoreButton.click()
+                                await new Promise(res => setTimeout(res, 100))
+                            }
+                        
+                            const isShortVideo = Array.from(element.querySelectorAll('span')).find(element => element.textContent.includes('Short Video')) ? true : false
+                            if (isShortVideo) {
+                                textElement = element.querySelector('.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x1vvkbs.x126k92a')
+                            } else {
+                                textElement = Array.from(element.querySelectorAll('[class=""]')).find(element => element.parentNode.children.length > 1).nextElementSibling.nextElementSibling
+                            }
+                        
+                            imagesArray = Array.from(element.querySelectorAll('img')).map(element => element.src).filter(url => url.includes('scontent'))
+                        
+                            authorElement = Array.from(element.querySelectorAll('a')).find(element => element.innerText && element.href.includes('user') && !element.closest('.xzueoph'))
+                        
+                            resolve({
+                                text: textElement.innerText,
+                                images: imagesArray,
+                                author: {
+                                    name: authorElement ? authorElement.innerText : null,
+                                    id: authorElement ? authorElement.href.split('/')[authorElement.href.split('/').indexOf('user') + 1] : null,
+                                    link: authorElement ? authorElement.href : null
+                                }
+                            })
+                        })
+                    })
+
+                    post.groupID = id
+
+                    posts.push(post)
+                }
+
+                resolve(posts)
+
+                page.close()
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+}
+
+module.exports = fb
+
+
+
+function extractDateFromPostElement(element) {
+    const seeMoreButton = Array.from(element.querySelectorAll('div')).find(element => element.innerText === 'See more' && !element.closest('.xzueoph'))
+    if (seeMoreButton) {
+        seeMoreButton.click()
+    }
+
+    const isShortVideo = Array.from(element.querySelectorAll('span')).find(element => element.textContent.includes('Short Video')) ? true : false
+    if (isShortVideo) {
+        textElement = element.querySelector('xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs x126k92a')
+    } else {
+        textElement = Array.from(element.querySelectorAll('[class=""]')).find(element => element.parentNode.children.length > 1).nextElementSibling.nextElementSibling
+    }
+
+    imagesArray = Array.from(element.querySelectorAll('img')).map(element => element.src).filter(url => url.includes('scontent'))
+
+    authorElement = Array.from(element.querySelectorAll('a')).find(element => element.innerText && element.href.includes('user') && !element.closest('.xzueoph'))
+
+    return {
+        text: textElement.innerText,
+        images: imagesArray,
+        author: {
+            name: authorElement ? authorElement.innerText : 'Annonomous Poster',
+            id: authorElement ? authorElement.href.split('/')[authorElement.href.split('/').indexOf('user') + 1] : null,
+            link: authorElement ? authorElement.href : null
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const fs = require('./fs-lib')
+const fb = require('./fb-lib')
+const fs = require('fs')
 require('dotenv').config()
 
-fs.login(process.env.FB_USER, process.env.FB_PASS).catch(error => console.log(error))
+fb.login(process.env.FB_USER, process.env.FB_PASS).catch(error => console.log(error))
     .then(async () => {
-        groups = await fs.getJoinedGroups()
+        const groups = await fb.getJoinedGroups()
 
-        console.log(groups)
+        // console.log(groups)
 
-        const posts = await fs.getGroupPosts(groups[0].id, 1)
+        // const posts = await fb.getGroupPosts(942846812392787, 30)
+        // console.log(posts)
 
-        console.log(posts)
+        for (const group of groups) {
+            console.log(`Grabbing 30 Posts from ${group.name}`)
+
+            const posts = await fb.getGroupPosts(group.id, 30)
+
+            console.log('Got Posts.')
+
+            fs.writeFileSync(`./scraped/${group.name}.txt`, JSON.stringify(posts, null, 4))
+        }
     })

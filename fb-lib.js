12a84e3f31a88ca915e1241f01b9d9404b207332
MrCraft18 <mariocaden@gmail.com>
2024-03-06 09:53:58 -0600

    Started making loop check functionality

--- fb-lib.js
+++ fb-lib.js
@@ -86,10 +86,15 @@ const fb = {
             }
         })
     },
-    getGroupPosts: (groupID, limit) => {
+    getGroupPosts: (groupID, { limit, dateRange } = {}) => {
         return new Promise(async (resolve, reject) => {
+            if (!limit && !dateRange) reject(new Error('Must Provide Limit or Date Range'))
+            if (dateRange && !dateRange.end) reject(new Error('Must Provide at least "End" Date for Date Range'))
+
+            if (dateRange && !dateRange.start) dateRange.start = new Date(0)
+
             try {
-                const page = await browser.newPage()
+                page = await browser.newPage()
                 await page.setViewport({ width: 1920, height: 1080 })
 
                 await Promise.all([
@@ -97,15 +102,7 @@ const fb = {
                     page.waitForNavigation()
                 ])
 
-                await page.evaluate(() => {
-                    const originalWriteText = navigator.clipboard.writeText
-                    navigator.clipboard.writeText = (text) => {
-                        window.lastCopiedText = text
-                        return originalWriteText.apply(this, arguments)
-                    }
-                })
-
-                const groupName = await page.evaluate(() => {
+                groupName = await page.evaluate(() => {
                     groupNameElement = document.querySelector('.x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1heor9g.xt0b8zv.x1xlr1w8')
 
                     return groupNameElement.innerText
@@ -114,116 +111,26 @@ const fb = {
 
                 const posts = []
 
-                for (i = 0; posts.length < limit; i++) {
-                    await page.waitForFunction(index => {
-                        const elements = Array.from(document.querySelectorAll(`[role="feed"] > .x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z`))
-                        return elements[index]
-                    }, { polling: 'mutation' }, i)
-
-                    const postElements = await page.$$(`[role="feed"] > .x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z`)
-                    let postElement = postElements[i]
-
-                    await postElement.hover()
-                    
-                    const isShortVideo = await postElement.evaluate(element => {
-                        const shortVideoElement = Array.from(element.querySelectorAll('span'))
-                            .find(element => element.innerText.includes('Short Video'))
-
-                        return shortVideoElement ? true : false
-                    })
-
-                    if (isShortVideo) {
-                        console.log('Skipping Short Video')
-                        continue
+                if (limit) {
+                    for (let i = 0; posts.length < limit; i++) {
+                        const post = await grabAndExtractPost(i)
+    
+                        if (post) posts.push(post)
                     }
+                } else if (dateRange) {
+                    let lastPostDate = new Date(8640000000000000)
 
-                    const [text, images, name, authorID, timestamp, postID] = await Promise.all([
-                        //Get Post Text
-                        new Promise(async resolve => {
-                            //Click See More Button (if its there)
-                            await postElement.evaluate(element => {
-                                const seeMoreButtons = Array.from(element.querySelectorAll('div')).filter(element => element.innerText === 'See more' && !element.closest('.xzueoph'))
-                                if (seeMoreButtons) seeMoreButtons.forEach(element => element.click())
-                            })
-                        
-                            //Wait until all Text is loaded
-                            await page.waitForFunction(index => {
-                                const elements = Array.from(document.querySelectorAll(`[role="feed"] > .x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z`))
-                                const postElement = elements[index]
-                            
-                                const seeMoreButton = Array.from(postElement.querySelectorAll('div')).find(element => element.innerText === 'See more' && !element.closest('.xzueoph'))
-                                if (seeMoreButton) return false
-                                if (!seeMoreButton) return true
-                            }, { polling: 'mutation' }, i)
+                    for (let i = 0; dateRange.end < lastPostDate; i++) {
+                        const post = await grabAndExtractPost(i)
 
-                            const text = await postElement.evaluate(element => {
-                                const textElement = Array.from(element.querySelectorAll('[class=""]'))
-                                .find(element => element.parentNode.children.length > 1)
-                                .nextElementSibling
-                                .nextElementSibling
+                        if (post) {
+                            if (post.timestamp > dateRange.start && post.timestamp < dateRange.end) posts.push(post)
 
-                                return textElement ? textElement.innerText : null
-                            })
+                            lastPostDate = post.timestamp
 
-                            resolve(text)
-                        }),
-                        //Get Post Images
-                        postElement.evaluate(element => {
-                            return Array.from(element.querySelectorAll('img')).map(element => element.src).filter(url => url.includes('scontent'))
-                        }),
-                        //Get Author Name
-                        postElement.evaluate(element => {
-                            const nameElement = Array.from(element.querySelectorAll('a')).find(element => element.innerText && element.href.includes('user') && !element.closest('.xzueoph'))
-                            return nameElement ? nameElement.innerText : null
-                        }),
-                        //Get Author ID
-                        postElement.evaluate(element => {
-                            const anchorElement = Array.from(element.querySelectorAll('a')).find(element => element.innerText && element.href.includes('user') && !element.closest('.xzueoph'))
-                            return anchorElement ? anchorElement.href.split('/')[anchorElement.href.split('/').indexOf('user') + 1] : null
-                        }),
-                        //Get Timestamp
-                        new Promise(async resolve => {
-                            const postTimeElement = await postElement.$('.x4k7w5x.x1h91t0o.x1h9r5lt.x1jfb8zj.xv2umb2.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1qrby5j')
-                            await postTimeElement.evaluate(element => element.scrollIntoView({block: 'center'}))
-                            await postTimeElement.hover()
-
-                            const dateElement = await page.waitForSelector('.x193iq5w.xeuugli.x13faqbe.x1vvkbs.x10flsy6.x1nxh6w3.x1sibtaa.xo1l8bm.xzsf02u')
-
-                            const rawStr = await dateElement.evaluate(element => element.innerText)
-
-                            const strArr = rawStr.split(' ')
-                            strArr.shift()
-                            strArr.splice(strArr.indexOf('at'), 1)
-
-                            const formattedStr = strArr.join(' ')
-
-                            resolve(new Date(formattedStr))
-                        }),
-                        //Get Post Link
-                        postElement.evaluate(element => {
-                            const postAnchorElement = Array.from(element.querySelectorAll('a'))
-                                .find(element => element.href.includes('/posts'))
-
-                            return postAnchorElement.href.split('/')[postAnchorElement.href.split('/').indexOf('posts') + 1]
-                        })
-                    ])
-
-                    console.log(name)
-
-                    posts.push({
-                        text,
-                        images,
-                        timestamp,
-                        id: postID,
-                        author: {
-                            name,
-                            id: authorID,
-                        },
-                        group: {
-                            id: groupID,
-                            name: groupName
+                            // console.log(lastPostDate)
                         }
-                    })
+                    }
                 }
 
                 resolve(posts)
@@ -233,6 +140,120 @@ const fb = {
                 reject(error)
             }
         })
+
+
+
+        async function grabAndExtractPost(index) {
+            await page.waitForFunction(index => {
+                const elements = Array.from(document.querySelectorAll(`[role="feed"] > .x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z`))
+                return elements[index]
+            }, { polling: 'mutation' }, index)
+
+            const postElements = await page.$$(`[role="feed"] > .x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z`)
+            let postElement = postElements[index]
+
+            await postElement.hover()
+            
+            const isShortVideo = await postElement.evaluate(element => {
+                const shortVideoElement = Array.from(element.querySelectorAll('span'))
+                    .find(element => element.innerText.includes('Short Video'))
+
+                return shortVideoElement ? true : false
+            })
+
+            if (isShortVideo) {
+                // console.log('Skipping Short Video')
+                return undefined
+            }
+
+            const [text, images, name, authorID, timestamp, postID] = await Promise.all([
+                //Get Post Text
+                new Promise(async resolve => {
+                    //Click See More Button (if its there)
+                    await postElement.evaluate(element => {
+                        const seeMoreButtons = Array.from(element.querySelectorAll('div')).filter(element => element.innerText === 'See more' && !element.closest('.xzueoph'))
+                        if (seeMoreButtons) seeMoreButtons.forEach(element => element.click())
+                    })
+                
+                    //Wait until all Text is loaded
+                    await page.waitForFunction(index => {
+                        const elements = Array.from(document.querySelectorAll(`[role="feed"] > .x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z`))
+                        const postElement = elements[index]
+                    
+                        const seeMoreButton = Array.from(postElement.querySelectorAll('div')).find(element => element.innerText === 'See more' && !element.closest('.xzueoph'))
+                        if (seeMoreButton) return false
+                        if (!seeMoreButton) return true
+                    }, { polling: 'mutation' }, index)
+
+                    const text = await postElement.evaluate(element => {
+                        const textElement = Array.from(element.querySelectorAll('[class=""]'))
+                        .find(element => element.parentNode.children.length > 1)
+                        .nextElementSibling
+                        .nextElementSibling
+
+                        return textElement ? textElement.innerText : null
+                    })
+
+                    resolve(text)
+                }),
+                //Get Post Images
+                postElement.evaluate(element => {
+                    return Array.from(element.querySelectorAll('img')).map(element => element.src).filter(url => url.includes('scontent'))
+                }),
+                //Get Author Name
+                postElement.evaluate(element => {
+                    const nameElement = Array.from(element.querySelectorAll('a')).find(element => element.innerText && element.href.includes('user') && !element.closest('.xzueoph'))
+                    return nameElement ? nameElement.innerText : null
+                }),
+                //Get Author ID
+                postElement.evaluate(element => {
+                    const anchorElement = Array.from(element.querySelectorAll('a')).find(element => element.innerText && element.href.includes('user') && !element.closest('.xzueoph'))
+                    return anchorElement ? anchorElement.href.split('/')[anchorElement.href.split('/').indexOf('user') + 1] : null
+                }),
+                //Get Timestamp
+                new Promise(async resolve => {
+                    const postTimeElement = await postElement.$('.x4k7w5x.x1h91t0o.x1h9r5lt.x1jfb8zj.xv2umb2.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1qrby5j')
+                    await postTimeElement.evaluate(element => element.scrollIntoView({block: 'center'}))
+                    await postTimeElement.hover()
+
+                    const dateElement = await page.waitForSelector('.x193iq5w.xeuugli.x13faqbe.x1vvkbs.x10flsy6.x1nxh6w3.x1sibtaa.xo1l8bm.xzsf02u')
+
+                    const rawStr = await dateElement.evaluate(element => element.innerText)
+
+                    const strArr = rawStr.split(' ')
+                    strArr.shift()
+                    strArr.splice(strArr.indexOf('at'), 1)
+
+                    const formattedStr = strArr.join(' ')
+
+                    resolve(new Date(formattedStr))
+                }),
+                //Get Post Link
+                postElement.evaluate(element => {
+                    const postAnchorElement = Array.from(element.querySelectorAll('a'))
+                        .find(element => element.href.includes('/posts'))
+
+                    return postAnchorElement.href.split('/')[postAnchorElement.href.split('/').indexOf('posts') + 1]
+                })
+            ])
+
+            // console.log(name)
+
+            return {
+                text,
+                images,
+                timestamp,
+                id: postID,
+                author: {
+                    name,
+                    id: authorID,
+                },
+                group: {
+                    id: groupID,
+                    name: groupName
+                }
+            }
+        }
     }
 }
 
--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 const fb = require('./fb-lib')
-const fs = require('fs')
+const { MongoClient, ObjectId } = require('mongodb')
 require('dotenv').config()
+
+const client = new MongoClient(process.env.MONGODB_URI)
+const leads = client.db('JV-FINDER').collection('leads')
 
 fb.login(process.env.FB_USER, process.env.FB_PASS).catch(error => console.log(error))
     .then(async () => {
         const groups = await fb.getJoinedGroups()
+        
+        //BEGIN LOOP
 
-        // console.log(groups)
+        listenForNewPosts((post) => {
+            
+        })
 
-        // const posts = await fb.getGroupPosts('1098785670138794', 30)
-        // console.log(posts)
+        let lastLeadTImestamp = await leads
+            .findOne({}, { 
+                projection: { 'post.timestamp': 1, _id: 0 },
+                sort: { 'post.timestamp': -1 }
+            })
+            //If response is valid return timestamp
+            //If response  is null return a date 8 hours before current date
+            .then(response => response ? response.post.timestamp : new Date(Date.now() - (1000 * 60 * 60 * 8)))
 
-        let allPosts = []
+        shuffleArray(groups)
 
         for (const group of groups) {
-            console.log(`Grabbing 30 Posts from ${group.name}`)
+            console.log(`Grabbing Posts from ${group.name}`)
 
-            const posts = await fb.getGroupPosts(group.id, 30)
+            const posts = await fb.getGroupPosts(group.id, {dateRange: {
+                // start: new Date('03/03/2024, 01:00:00 PM'),
+                end: lastLeadTImestamp
+            }})
 
-            console.log('Got Posts.')
-
-            allPosts = [...posts, ...allPosts]
+            //PROCESS POSTS
         }
-
-        fs.writeFileSync(`./scraped/all-posts.txt`, JSON.stringify(allPosts, null, 4))
     })
+
+
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        // Generate a random index from 0 to i
+        const j = Math.floor(Math.random() * (i + 1));
+        // Swap elements at indices i and j
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
